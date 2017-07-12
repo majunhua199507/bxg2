9ff7232129c8381c471c226c11dc81c5c7354b20
342344403@qq.com <342344403@qq.com>
2017-07-12 11:51:21 +0800

    增加了删除功能

--- bxg/public/js/common.js
+++ bxg/public/js/common.js
@@ -15,4 +15,16 @@ define(['jquery','cookie','template'], function ($,cookie,template) {
 		var htmlStr = template('tpl', tcInfo);
 		$('.aside>.profile').html(htmlStr);
 	}
+	$('#out').on('click',function () {
+		$.ajax({
+			url:'/api/logout',
+			type:'post',
+			success:function (info) {
+				if (info.code==200) {
+					alert('成功退出'),
+					location.href='login'
+				}
+			}
+		})
+	})
 })
